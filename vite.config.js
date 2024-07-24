f053db5c8bd56eb288f6b8062976fca62d3a034e
MartinMoralesGithub <mm.colagiovanni86@gmail.com>
2024-07-24 18:29:40 -0300

    vercel4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {rollupOptions: {
+      // ... other options
+      output: {chunkSizeWarningLimit: 1000000, // Set warning limit to 1 MB},
+    },},},
 })
